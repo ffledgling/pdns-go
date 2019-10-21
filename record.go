d95f05e1f9cf43c2762b098be08f782d25b1dc55
Anhad Jai Singh (via automation) <ffledgling+travis-pdns-go@gmail.com>
2019-10-21 08:32:06 +0000

    Automated release of pdns-go bindings

--- record.go
+++ record.go
@@ -15,8 +15,8 @@ type Record struct {
 	// The content of this record
 	Content string `json:"content"`
 
-	// Whether or not this record is disabled
-	Disabled bool `json:"disabled"`
+	// Whether or not this record is disabled. When unset, the record is not disabled
+	Disabled bool `json:"disabled,omitempty"`
 
 	// If set to true, the server will find the matching reverse zone and create a PTR there. Existing PTR records are replaced. If no matching reverse Zone, an error is thrown. Only valid in client bodies, only valid for A and AAAA types. Not returned by the server. This feature is deprecated and will be removed in 4.3.0.
 	SetPtr bool `json:"set-ptr,omitempty"`
